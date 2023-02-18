7d66d07de3de4f01d213a1b8dff041ad5b5b3c31
Angel Garcia <tessio1994@gmail.com>
2023-02-18 01:05:21 +0100

    Prueba js

--- js/custom.js
+++ js/custom.js
@@ -28,45 +28,47 @@ jQuery(document).ready(function(){
 
   //
   
-  // $("#1 > a").click(function(){
-  //   $(this).parent().addClass("customClass");
-  //   $("#2").removeClass('customClass');
-  //   $("#3").removeClass('customClass');
-  //   $("#4").removeClass('customClass');
-  // });
+  $("#1 > a").click(function(){
+    $(this).parent().addClass("customClass");
+    $("#2").removeClass('customClass');
+    $("#3").removeClass('customClass');
+    $("#4").removeClass('customClass');
+  });
 
-  // $("#2 > a").click(function(){
-  //   $(this).parent().addClass("customClass");
-  //   $("#1").removeClass('customClass');
-  //   $("#3").removeClass('customClass');
-  //   $("#4").removeClass('customClass');
-  // });
+  $("#2 > a").click(function(){
+    $(this).parent().addClass("customClass");
+    $("#1").removeClass('customClass');
+    $("#3").removeClass('customClass');
+    $("#4").removeClass('customClass');
+  });
 
-  // $("#3 > a").click(function(){
-  //   $(this).parent().addClass("customClass");
-  //   $("#1").removeClass('customClass');
-  //   $("#2").removeClass('customClass');
-  //   $("#4").removeClass('customClass');
-  // });
+  $("#3 > a").click(function(){
+    $(this).parent().addClass("customClass");
+    $("#1").removeClass('customClass');
+    $("#2").removeClass('customClass');
+    $("#4").removeClass('customClass');
+  });
 
-  // $("#4 > a").click(function(){
-  //   $(this).parent().addClass("customClass");
-  //   $("#1").removeClass('customClass');
-  //   $("#2").removeClass('customClass');
-  //   $("#3").removeClass('customClass');
-  // });
+  $("#4 > a").click(function(){
+    $(this).parent().addClass("customClass");
+    $("#1").removeClass('customClass');
+    $("#2").removeClass('customClass');
+    $("#3").removeClass('customClass');
+  });
 
   $(function() {
     var loc = window.location.href; // returns the full URL
     if(/#inicio/.test(loc)) {
       $('#1').addClass('customClass');
     } 
-  });
-
-  $(function() {
-    var loc = window.location.href; // returns the full URL
     if(/#prueba1/.test(loc)) {
       $('#2').addClass('customClass');
     }  
+    if(/#prueba2/.test(loc)) {
+      $('#3').addClass('customClass');
+    } 
+    if(/#prueba3/.test(loc)) {
+      $('#4').addClass('customClass');
+    }
   });
 });
